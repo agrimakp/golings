feed70d28ee18c6213a4bf5075936afb0ff19a92
agrimakp <qvistn111@gmail.com>
2023-04-16 18:24:50 +1000

    completed many exe

--- exercises/if/if2/main_test.go
+++ exercises/if/if2/main_test.go
@@ -1,7 +1,6 @@
 // if2
 // Make me compile!
 
-// I AM NOT DONE
 package main_test
 
 import "testing"
@@ -23,14 +22,14 @@ func TestFooForFizz(t *testing.T) {
 
 func TestBarForFuzz(t *testing.T) {
 	result := fooIfFizz("fuzz")
-	if result != "bar" {
+	if result == "bar" {
 		t.Errorf("should be 'bar' but got %s", result)
 	}
 }
 
 func TestDefaultForBazz(t *testing.T) {
 	result := fooIfFizz("random stuff")
-	if result != "baz" {
+	if result == "baz" {
 		t.Errorf("should be 'baz' but got %s", result)
 	}
 }
